c418c2f0af7496372ad8dc2b9e4b5f6fd01a7055
Doug Keddie <dkeddie@gpstrategies.com>
2020-02-10 10:00:57 -0500

    Main ViewModel and base class created

---
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace FriendOrganizer.UI.ViewModel
+{
+    using System.Collections.ObjectModel;
+    using FriendOrganizer.Model;
+    using FriendOrganizer.UI.Data;
+
+    public class MainViewModel : ViewModelBase
+    {
+        private IFriendDataService friendDataService;
+        private Friend selectedFriend;
+
+        public MainViewModel(IFriendDataService friendDataService)
+        {
+            Friends = new ObservableCollection<Friend>();
+            this.friendDataService = friendDataService;
+        }
+
+        public void Load()
+        {
+            var friends = friendDataService.GetAll();
+            Friends.Clear();
+
+            foreach (var friend in friends)
+            {
+                Friends.Add(friend);
+            }
+        }
+
+        public ObservableCollection<Friend> Friends { get; set; }
+
+        public Friend SelectedFriend
+        {
+            get { return selectedFriend; }
+            set
+            {
+                selectedFriend = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ FriendOrganizer.UI/ViewModel/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿namespace FriendOrganizer.UI.ViewModel
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

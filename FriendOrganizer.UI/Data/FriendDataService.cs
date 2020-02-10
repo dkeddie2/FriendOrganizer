0e550d803ca2cdfe03f2de46dd9c4ba10ccdbca2
Doug Keddie <dkeddie@gpstrategies.com>
2020-02-10 09:48:28 -0500

    Added fake DataService

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,16 @@
+﻿namespace FriendOrganizer.UI.Data
+{
+    using System.Collections.Generic;
+    using FriendOrganizer.Model;
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            // TODO: Load data from a real database.
+            yield return new Friend { FirstName = "Thomas", LastName = "Huber" };
+            yield return new Friend { FirstName = "Andreas", LastName = "Boehler" };
+            yield return new Friend { FirstName = "Julia", LastName = "Huber" };
+            yield return new Friend { FirstName = "Chrissi", LastName = "Egin" };
+        }
+    }
+}
---
+++ FriendOrganizer.UI/Data/IFriendDataService.cs
@@ -0,0 +1,10 @@
+﻿namespace FriendOrganizer.UI.Data
+{
+    using System.Collections.Generic;
+    using FriendOrganizer.Model;
+
+    public interface IFriendDataService
+    {
+        IEnumerable<Friend> GetAll();
+    }
+}

2034fb4d51906139e66a684f7b9aebaf0b0d213e
Doug Keddie <dkeddie@gpstrategies.com>
2020-02-10 15:13:25 -0500

    Added validation on FirstName field via Wrapper class on Friend model and INotifyDataErrorInfo

--- FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
+++ FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
@@ -1,11 +1,10 @@
 ﻿namespace FriendOrganizer.UI.ViewModel
 {
-    using System;
     using System.Threading.Tasks;
     using System.Windows.Input;
-    using FriendOrganizer.Model;
     using FriendOrganizer.UI.Data;
     using FriendOrganizer.UI.Event;
+    using FriendOrganizer.UI.Wrapper;
     using Prism.Commands;
     using Prism.Events;
 
@@ -13,7 +12,7 @@
     {
         private IFriendDataService dataService;
         private IEventAggregator eventAggregator;
-        private Friend friend;
+        private FriendWrapper friend;
 
         public FriendDetailViewModel(IFriendDataService dataService, IEventAggregator eventAggregator)
         {
@@ -24,17 +23,7 @@
             SaveCommand = new DelegateCommand(OnSaveExecute, OnSaveCanExecute);
         }
 
-        private async void OnOpenFriendDetailView(int friendId)
-        {
-            await LoadAsync(friendId);
-        }
-
-        public async Task LoadAsync(int friendId)
-        {
-            Friend = await dataService.GetByIdAsync(friendId);
-        }
-
-        public Friend Friend
+        public FriendWrapper Friend
         {
             get { return friend; }
             private set
@@ -44,16 +33,17 @@
             }
         }
 
-        private async void OnSaveExecute()
+        public ICommand SaveCommand { get; }
+
+        public async Task LoadAsync(int friendId)
         {
-            await dataService.SaveAsync(Friend);
-            eventAggregator.GetEvent<AfterFriendSavedEvent>()
-                .Publish(
-                new AfterFriendSavedEventArgs
-                {
-                    Id = Friend.Id,
-                    DisplayMember = $"{Friend.FirstName} {Friend.LastName}",
-                });
+            var friend = await dataService.GetByIdAsync(friendId);
+            Friend = new FriendWrapper(friend);
+        }
+
+        private async void OnOpenFriendDetailView(int friendId)
+        {
+            await LoadAsync(friendId);
         }
 
         private bool OnSaveCanExecute()
@@ -62,6 +52,16 @@
             return true;
         }
 
-        public ICommand SaveCommand { get; }
+        private async void OnSaveExecute()
+        {
+            await dataService.SaveAsync(Friend.Model);
+            eventAggregator.GetEvent<AfterFriendSavedEvent>()
+                .Publish(
+                new AfterFriendSavedEventArgs
+                {
+                    Id = Friend.Id,
+                    DisplayMember = $"{Friend.FirstName} {Friend.LastName}",
+                });
+        }
     }
 }
---
+++ FriendOrganizer.UI/Wrapper/FriendWrapper.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using FriendOrganizer.Model;
+using FriendOrganizer.UI.ViewModel;
+
+namespace FriendOrganizer.UI.Wrapper
+{
+    public class FriendWrapper : ViewModelBase, INotifyDataErrorInfo
+    {
+        private Dictionary<string, List<string>> errorsByPropertyName = new Dictionary<string, List<string>>();
+
+        public FriendWrapper(Friend model)
+        {
+            Model = model;
+        }
+
+        public Friend Model { get; }
+
+        public int Id { get { return Model.Id; } }
+
+        public string FirstName
+        {
+            get { return Model.FirstName; }
+            set
+            {
+                Model.FirstName = value;
+                OnPropertyChanged();
+                ValidateProperty(nameof(FirstName));
+            }
+        }
+
+        public string LastName
+        {
+            get { return Model.LastName; }
+            set
+            {
+                Model.LastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Email
+        {
+            get { return Model.Email; }
+            set
+            {
+                Model.Email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        void ValidateProperty(string propertyName)
+        {
+            ClearErrors(propertyName);
+            switch (propertyName)
+            {
+                case nameof(FirstName):
+                    if (string.Equals(FirstName, "Robot", StringComparison.OrdinalIgnoreCase))
+                    {
+                        AddError(propertyName, "Robots are not valid friends");
+                    }
+                    break;
+            }
+        }
+
+        public bool HasErrors => errorsByPropertyName.Any();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return errorsByPropertyName.ContainsKey(propertyName) ? errorsByPropertyName[propertyName] : null;
+        }
+
+        private void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        private void AddError(string propertyName, string error)
+        {
+            if (errorsByPropertyName.ContainsKey(propertyName) == false)
+            {
+                errorsByPropertyName[propertyName] = new List<string>();
+            }
+            if (errorsByPropertyName[propertyName].Contains(error) == false)
+            {
+                errorsByPropertyName[propertyName].Add(error);
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        private void ClearErrors(string propertyName)
+        {
+            if (errorsByPropertyName.ContainsKey(propertyName))
+            {
+                errorsByPropertyName.Remove(propertyName);
+                OnErrorsChanged(propertyName);
+            }
+        }
+    }
+}

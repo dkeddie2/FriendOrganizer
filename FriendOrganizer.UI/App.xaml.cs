58d7d404436a335f7130add65549d4a56432b5dc
Doug Keddie <dkeddie@gpstrategies.com>
2020-02-10 10:17:48 -0500

    Added code needed for startup via Startup event in App.xaml

--- FriendOrganizer.UI/App.xaml.cs
+++ FriendOrganizer.UI/App.xaml.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace FriendOrganizer.UI
+﻿namespace FriendOrganizer.UI
 {
+    using System.Windows;
+    using FriendOrganizer.UI.Data;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = new MainWindow(
+                new ViewModel.MainViewModel(
+                    new FriendDataService()));
+
+            mainWindow.Show();
+        }
     }
 }

08d94458783ff1a1b3e273b5199b98cf366f511c
Doug Keddie <dkeddie@gpstrategies.com>
2020-02-10 10:53:45 -0500

    Adding Constraints to Entity Framework via DataAttributes on model.  Re-scaffolded database structure by running Add-Migration again

--- FriendOrganizer.Model/Friend.cs
+++ FriendOrganizer.Model/Friend.cs
@@ -1,13 +1,19 @@
 ﻿namespace FriendOrganizer.Model
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class Friend
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(50)]
         public string FirstName { get; set; }
 
+        [StringLength(50)]
         public string LastName { get; set; }
 
+        [StringLength(50)]
         public string Email { get; set; }
     }
 }
